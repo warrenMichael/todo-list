367f6af7d351312afe3f549e8e72305a7aedb487
Michael Warren <Michael_Warren@condenast.com>
2019-07-26 18:16:18 -0400

    Updates

---
+++ precache-manifest.5106eccd3240e03e807fcce96ff28f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461e905051f9b43b9d42a089af4db52f",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "52e781e0c321e624f116",
+    "url": "/todo-list/static/css/main.4160bff0.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/todo-list/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "52e781e0c321e624f116",
+    "url": "/todo-list/static/js/main.49658870.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
